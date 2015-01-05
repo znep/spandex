f4cce1036c2311804e5c913c863a4559b4c5b2f9
John Krah <john.krah@socrata.com>
2015-01-05 14:04:19 -0800

    es install steps

--- spike/install-elasticsearch.sh
+++ spike/install-elasticsearch.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
-### install elasticsearch 
+### install elasticsearch
 
-wget -qO - http://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add - 
-sudo echo 'deb http://packages.elasticsearch.org/elasticsearch/1.4/debian stable main' >> /etc/apt/sources.list 
-sudo apt-get update 
-sudo apt-get install elasticsearch 
-
-sudo add-apt-repository ppa:webupd8team/java 
-sudo apt-get update 
-sudo apt-get install oracle-java7-installer 
-java -version 
+wget -qO - http://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add -
+sudo echo 'deb http://packages.elasticsearch.org/elasticsearch/1.4/debian stable main' >> /etc/apt/sources.list
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install elasticsearch oracle-java7-installer
+java -version
 
 ### start directly, or add service startup
-sudo /etc/init.d/elasticsearch start 
-# sudo update-rc.d elasticsearch defaults 95 10 
+sudo /etc/init.d/elasticsearch start
+# sudo update-rc.d elasticsearch defaults 95 10
 
