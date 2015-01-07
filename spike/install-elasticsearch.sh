03c4231e549a781daf140d75997553ee5400f4bf
John Krah <john.krah@socrata.com>
2015-01-07 12:28:21 -0800

    config memory

--- spike/install-elasticsearch.sh
+++ spike/install-elasticsearch.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 ### install elasticsearch
 
+ES_PACK='deb http://packages.elasticsearch.org/elasticsearch/1.4/debian stable main'
+ES_CONF=/etc/elasticsearch/elasticsearch.yml
+ES_INIT=/etc/init.d/elasticsearch
+ES_HEAP='31g'
+
 wget -qO - http://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add -
-echo 'deb http://packages.elasticsearch.org/elasticsearch/1.4/debian stable main' | sudo tee -a /etc/apt/sources.list
+echo $ES_PACK | sudo tee -a /etc/apt/sources.list >/dev/null
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install elasticsearch oracle-java7-installer
 java -version
+
+echo 'bootstrap.mlockall: true' | sudo tee -a $ES_CONF >/dev/null
+cat $ES_INIT | sed "s/^#ES_HEAP_SIZE=.*$/ES_HEAP_SIZE=$ES_HEAP/" | sudo tee $ES_INIT >/dev/null
 
 ### start directly, or add service startup
 sudo /etc/init.d/elasticsearch start
