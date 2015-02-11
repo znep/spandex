656f80ea2bad3868191b60e82957357220d9a481
John Krah <john.krah@socrata.com>
2015-02-11 10:10:31 -0800

    wireup add dataset to elasticsearch put mapping

--- src/main/scala/com/socrata/spandex/SpandexServlet.scala
+++ src/main/scala/com/socrata/spandex/SpandexServlet.scala
@@ -5,7 +5,59 @@ import scala.concurrent.duration.Duration
 import scala.concurrent.Await
 import wabisabi.{Client => ElasticsearchClient}
 
-class SpandexServlet(elasticsearch: ElasticsearchClient) extends SpandexStack {
+class SpandexServlet(esc: ElasticsearchClient) extends SpandexStack {
+  private val indices = Seq("spandex")
+  private val indexSettings =
+    """
+      |{
+      |  "settings": {
+      |    "index": {
+      |      "number_of_shards": 2,
+      |      "number_of_replicas": 1,
+      |      "refresh_interval": "-1",
+      |      "translog": {
+      |        "flush_threshold_size": "1g"
+      |      }
+      |    }
+      |  }
+      |}
+    """.stripMargin
+  private def ensureIndex(index: String): String = {
+    val indexResponse = Await.result(esc.verifyIndex(index), Duration("1s"))
+    val resultHttpCode = indexResponse.getStatusCode
+    if (resultHttpCode == HttpStatus.SC_NOT_FOUND) {
+      Await.result(esc.createIndex(index, Some(indexSettings)), Duration("1s")).getResponseBody
+    } else {
+      indexResponse.getResponseBody
+    }
+  }
+
+  private val mappingBase =
+    """
+      |{
+      |    "%s": {
+      |        "_source": {"enabled": false},
+      |        "_all": {"enabled": false},
+      |        "properties": {
+      |            %s
+      |        }
+      |    }
+      |}
+    """.stripMargin
+  private val mappingCol =
+    """
+      |"%s": {
+      |    "type": "completion",
+      |    "index_analyzer": "simple",
+      |    "search_analyzer": "simple",
+      |    "payloads": false,
+      |    "preserve_separators": false,
+      |    "preserve_position_increments": false,
+      |    "max_input_length": 30720
+      |}
+    """.stripMargin
+  private def mapping(fourbyfour: String, columns: Seq[String]): String =
+     mappingBase.format(fourbyfour, columns.map(mappingCol.format(_)).mkString(","))
 
   get("//?") {
     // TODO: spandex getting started and/or quick reference
@@ -17,18 +69,24 @@ class SpandexServlet(elasticsearch: ElasticsearchClient) extends SpandexStack {
   }
 
   get ("/health/?"){
-    val response = Await.result(elasticsearch.health(level = Some("cluster")), Duration("1s"))
+    val response = Await.result(esc.health(level = Some("cluster")), Duration("1s"))
     response.getResponseBody
   }
 
   get ("/add/:4x4/?"){
     val fourbyfour = params.getOrElse("4x4", halt(HttpStatus.SC_BAD_REQUEST))
-    // TODO: elasticsearch add mappings
-    """{
-      | "acknowledged":true,
-      | "4x4":"%s"
-      |}
-    """.stripMargin.format(fourbyfour)
+    // TODO: elasticsearch add index routing
+    indices.map(ensureIndex)
+    val previousMapping = Await.result(esc.getMapping(indices, Seq(fourbyfour)), Duration("1s")).getResponseBody
+    val newMapping = mapping(fourbyfour, Seq.empty)
+    if (previousMapping != newMapping) {
+      Await.result(esc.putMapping(indices, fourbyfour, newMapping), Duration("1s")).getResponseBody
+    } else {
+      """{
+        | "acknowledged":true
+        |}
+      """.stripMargin.format(fourbyfour)
+    }
   }
 
   get ("/add/:4x4/:col/?"){
--- src/test/scala/com/socrata/spandex/SpandexServletSpec.scala
+++ src/test/scala/com/socrata/spandex/SpandexServletSpec.scala
@@ -47,7 +47,6 @@ class SpandexServletSpec extends ScalatraSuite with FunSuiteLike {
     get("/add/qnmj-8ku6"){
       status should equal (HttpStatus.SC_OK)
       body should include (acknowledged)
-      body should include ("\"4x4\":\"qnmj-8ku6\"")
     }
   }
 
