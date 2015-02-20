63116ca673b657cf1e0c43ab607bd7d6f9a6b1c5
John Krah <john.krah@socrata.com>
2015-02-20 12:06:26 -0800

    reduce config path repitition

--- src/main/scala/com/socrata/spandex/SpandexConfig.scala
+++ src/main/scala/com/socrata/spandex/SpandexConfig.scala
@@ -1,16 +1,15 @@
 package com.socrata.spandex
 
 import com.typesafe.config.{ConfigFactory, Config}
-import scala.collection.JavaConversions._
 import scala.concurrent.duration.Duration
 
-class SpandexConfig(conf: Config = ConfigFactory.load()) {
-  val port: Int = conf.getInt("spandex.port")
-  val esUrl: String = conf.getString("spandex.elasticsearch.url")
-  val index: String = conf.getString("spandex.elasticsearch.index")
-  val indexSettings: String = conf.getString("spandex.elasticsearch.indexSettings")
-  val indexBaseMapping: String = conf.getString("spandex.elasticsearch.indexBaseMapping")
-  val indexColumnMapping: String = conf.getString("spandex.elasticsearch.indexColumnMapping")
-  val escTimeoutFast: Duration = Duration(conf.getString("spandex.elasticsearch.esClientTimeoutFast"))
-  val escTimeout: Duration = Duration(conf.getString("spandex.elasticsearch.esClientTimeoutContent"))
+class SpandexConfig(conf: Config = ConfigFactory.load().getConfig("com.socrata.spandex")) {
+  val port: Int = conf.getInt("port")
+  val esUrl: String = conf.getString("elasticsearch.url")
+  val index: String = conf.getString("elasticsearch.index")
+  val indexSettings: String = conf.getString("elasticsearch.indexSettings")
+  val indexBaseMapping: String = conf.getString("elasticsearch.indexBaseMapping")
+  val indexColumnMapping: String = conf.getString("elasticsearch.indexColumnMapping")
+  val escTimeoutFast: Duration = Duration(conf.getString("elasticsearch.esClientTimeoutFast"))
+  val escTimeout: Duration = Duration(conf.getString("elasticsearch.esClientTimeoutContent"))
 }
