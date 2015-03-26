2e68aa93559a8d56f8329bc0350af22809a79be1
John Krah <john.krah@socrata.com>
2015-03-26 15:28:13 -0700

    verify config options are defined

--- spandex-common/src/test/scala/com/socrata/spandex/common/SpandexConfigSpec.scala
+++ spandex-common/src/test/scala/com/socrata/spandex/common/SpandexConfigSpec.scala
@@ -5,19 +5,18 @@ import org.scalatest.{FunSuiteLike, Matchers}
 class SpandexConfigSpec extends FunSuiteLike with Matchers {
   test("spandex config has these required values") {
     val conf = new SpandexConfig
-    conf.spandexPort
+    Some(conf.spandexPort) should be ('defined)
   }
 
   test("elasticsearch config has these required values") {
     val conf = new SpandexConfig().es
-    (
-      conf.host,
-      conf.port,
-      conf.clusterName,
-      conf.index,
-      conf.fieldValueMapping,
-      conf.columnMapMapping,
-      conf.datasetCopyMapping
-      )
+    Some(conf.host) should be ('defined)
+    Some(conf.port) should be ('defined)
+    Some(conf.clusterName) should be ('defined)
+    Some(conf.index) should be ('defined)
+    Some(conf.fieldValueMapping) should be ('defined)
+    Some(conf.columnMapMapping) should be ('defined)
+    Some(conf.datasetCopyMapping) should be ('defined)
+
   }
 }
