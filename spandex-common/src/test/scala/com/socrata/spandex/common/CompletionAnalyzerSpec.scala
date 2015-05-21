e4d1694c76cb45ef27b2f34a745a2cc506f108fb
John Krah <john.krah@socrata.com>
2015-05-21 15:18:40 -0700

    separate analyzer tests

--- spandex-common/src/test/scala/com/socrata/spandex/common/CompletionAnalyzerSpec.scala
+++ spandex-common/src/test/scala/com/socrata/spandex/common/CompletionAnalyzerSpec.scala
@@ -1,62 +1,13 @@
 package com.socrata.spandex.common
 
 import com.rojoma.json.v3.util.JsonUtil
-import com.socrata.datacoordinator.secondary.LifecycleStage
 import com.socrata.spandex.common.client._
-import com.typesafe.config.ConfigFactory
-import org.elasticsearch.common.unit.Fuzziness
-import org.elasticsearch.search.suggest.Suggest.Suggestion
-import org.elasticsearch.search.suggest.completion.CompletionSuggestion.Entry
 import org.scalatest.{BeforeAndAfterAll, FunSuiteLike, Matchers}
 
-import scala.collection.JavaConversions._
-import scala.collection.mutable
-
-class CompletionAnalyzerSpec extends FunSuiteLike with Matchers with BeforeAndAfterAll {
-  val baseConfig = ConfigFactory.load().getConfig("com.socrata.spandex")
-  val config = new SpandexConfig(
-    ConfigFactory.parseString(
-      """{
-        |analysis {
-        |  enabled = true
-        |  lucene-version = "4.10.3"
-        |  max-input-length = 64
-        |  max-shingle-length = 32
-        |}
-      |}""".stripMargin)
-      .withFallback(baseConfig)
-  )
-  val client = new TestESClient(config.es)
-  CompletionAnalyzer.configure(config.analysis)
-
-  override protected def beforeAll(): Unit = {
-    super.beforeAll()
-    SpandexBootstrap.ensureIndex(config.es, client)
-  }
-
-  override protected def afterAll(): Unit = {
-    super.afterAll()
-  }
-
-  private val ds = "ds.one"
-  private val copy = DatasetCopy(ds, 1, 42, LifecycleStage.Published)
-  private val col = ColumnMap(copy.datasetId, copy.copyNumber, 2, "column2")
-
-  private var docId = 10
-  private def index(value: String): Unit =
-    index(FieldValue(col.datasetId, col.copyNumber, col.systemColumnId, docId, value))
-  private def index(fv: FieldValue): Unit = {
-    client.indexFieldValue(fv, refresh = true)
-    docId += 1
-  }
-
-  private def suggest(query: String, fuzz: Fuzziness = Fuzziness.ZERO): mutable.Buffer[String] = {
-    val response = client.suggest(col, 10, query, fuzz, 2, 1)
-    val suggest = response.getSuggestion[Suggestion[Entry]]("suggest")
-    val entries = suggest.getEntries
-    val options = entries.get(0).getOptions
-    options.map(_.getText.toString)
-  }
+class CompletionAnalyzerSpec extends FunSuiteLike with Matchers with AnalyzerTest with BeforeAndAfterAll {
+  override val analyzerEnabled: Boolean = true
+  override protected def beforeAll(): Unit = analyzerBeforeAll()
+  override protected def afterAll(): Unit = analyzerAfterAll()
 
   // relocated from SpandexElasticSearchClientSpec
   test("suggest is case insensitive and supports numbers and symbols") {
@@ -91,7 +42,7 @@ class CompletionAnalyzerSpec extends FunSuiteLike with Matchers with BeforeAndAf
     suggestionsSym should contain(sym.value)
   }
 
-  test("settings include custom analyzers") {
+  test("settings include custom analyzer") {
     val clusterState = client.client.admin().cluster().prepareState().setIndices(config.es.index)
       .execute().actionGet().getState
     val indexMetadata = clusterState.getMetaData.index(config.es.index)
@@ -161,10 +112,10 @@ class CompletionAnalyzerSpec extends FunSuiteLike with Matchers with BeforeAndAf
     suggest("completion") should contain(expectedValue)
     suggest("suggester") should contain(expectedValue)
     suggest("html") should contain(expectedValue)
-    suggest("lucene.rocks") should contain(expectedValue)
+    suggest("lucene rocks") should contain(expectedValue)
   }
 
-  test("analysis length limits don't cutoff terms") {
+  test("completion pre analyzer length limits don't cutoff terms") {
     // a fictional dish mentioned in Aristophanes' comedy Assemblywomen
     val value = "Lopadotemachoselachogaleokranioleipsanodrimhypotrimmatosilphioparaomelitokatakechymenokichlepikossyphophattoperisteralektryonoptekephalliokigklopeleiolagoiosiraiobaphetraganopterygon"
     val expectedInputValue = value.toLowerCase
@@ -172,7 +123,7 @@ class CompletionAnalyzerSpec extends FunSuiteLike with Matchers with BeforeAndAf
     tokens.head should equal(expectedInputValue)
   }
 
-  test("limit analysis input string to a certain length") {
+  test("completion pre analyzer limit input string length") {
     val value = "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife. However little known the feelings or views of such a man may be on his first entering a neighbourhood, this truth is so well fixed in the minds of the surrounding families, that he is considered the rightful property of some one or other of their daughters. My dear Mr. Bennet, said his lady to him one day, have you heard that Netherfield Park is let at last? Mr. Bennet replied that he had not. But it is, returned she; for Mrs. Long has just been here, and she told me all about it. Mr. Bennet made no answer. Do you not want to know who has taken it? cried his wife impatiently. You want to tell me, and I have no objection to hearing it."
     // the config value 64 limits to -------------------------------------------^
     //                plus 14 spaces ---- - - - - - - - - - - - - - - - - - - - - - - - - - ---^
@@ -195,22 +146,5 @@ class CompletionAnalyzerSpec extends FunSuiteLike with Matchers with BeforeAndAf
       "it is a truth universally acknowledged")
     // the config value 32 limits to -^ plus spaces, and a little more to preserve terms
     CompletionAnalyzer.analyze(value) should equal(expectedInputValues)
-  }
-
-  val configOff = new SpandexConfig(
-    ConfigFactory.parseString(
-      """{
-        |analysis {
-        |  enabled = false
-        |}
-        |}""".stripMargin)
-      .withFallback(baseConfig)
-  )
-  test("completion pre analyzer disabled") {
-    CompletionAnalyzer.configure(configOff.analysis)
-    val value = "A phrase is a group of related words that does not include a subject and verb. (If the group of related words does contain a subject and verb, it is considered a clause.) There are several different kinds of phrases."
-    val expectedTokens = List(value)
-    val tokens = CompletionAnalyzer.analyze(value)
-    tokens should equal(expectedTokens)
   }
 }
