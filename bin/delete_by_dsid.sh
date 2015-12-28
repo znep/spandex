d0adb7d5a59533a04297eca8f7b0d4e258bb82c1
John Krah <john.krah@socrata.com>
2015-12-28 11:01:57 -0800

    all the cluster urls

--- bin/delete_by_dsid.sh
+++ bin/delete_by_dsid.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-CLUSTER="http://spandex-3.elasticsearch.aws-us-west-2-prod.socrata.net"
-delete_query="{\"query\":{\"term\":{\"dataset_id\":{\"value\":\"alpha.$1\"}}}}"
+CLUSTER="http://localhost:9200"
+#CLUSTER="http://spandex.elasticsearch.aws-us-west-2-staging.socrata.net"
+#CLUSTER="http://spandex.elasticsearch.aws-us-west-2-rc.socrata.net"
+#CLUSTER="http://spandex-3.elasticsearch.aws-us-west-2-prod.socrata.net"
+#CLUSTER="http://spandex.elasticsearch.aws-eu-west-1-prod.socrata.net"
+#CLUSTER="http://spandex.elasticsearch.aws-us-east-1-fedramp-prod.socrata.net"
 
+delete_query="{\"query\":{\"term\":{\"dataset_id\":{\"value\":\"alpha.$1\"}}}}"
 copy_count=$(curl -s $CLUSTER/spandex/dataset_copy/_search?size=0 -d $delete_query |jq '.hits.total')
 column_count=$(curl -s $CLUSTER/spandex/column_map/_search?size=0 -d $delete_query |jq '.hits.total')
 value_count=$(curl -s $CLUSTER/spandex/field_value/_search?size=0 -d $delete_query |jq '.hits.total')
+
 echo "found $copy_count copies, $column_count columns, $value_count values; deleting alpha.$1"
 curl -XDELETE $CLUSTER/spandex/_query -d $delete_query
 echo
