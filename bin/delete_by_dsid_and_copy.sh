7ea2038858d9f0d5af8c52a4d5b2466a8f1748a6
Robert Voyer <robert.voyer@gmail.com>
2017-01-25 13:31:49 -0800

    Add cleanup scripts for deleting datasets by copy and deleting garbage (Nessus) indices (#88)
    Reviewed-by: megan.schneider

---
+++ bin/delete_by_dsid_and_copy.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+STACK=$1
+DCU=alpha # data coordinator universe or whatever
+DSID=$2
+COPY_NUMBER=$3
+
+case $STACK in
+  "local" )
+    DCU=primus
+    CLUSTER="http://localhost:9200" ;;
+  "staging" )
+    CLUSTER="http://spandex.elasticsearch.aws-us-west-2-staging.socrata.net" ;;
+  "rc" )
+    CLUSTER="http://spandex.elasticsearch.aws-us-west-2-rc.socrata.net" ;;
+  "prod" )
+    CLUSTER="http://spandex-5.elasticsearch.aws-us-west-2-prod.socrata.net" ;;
+  "eu-prod" )
+    CLUSTER="http://spandex.elasticsearch.aws-eu-west-1-prod.socrata.net" ;;
+  "fedramp-prod" )
+    CLUSTER="http://spandex-6.elasticsearch.aws-us-east-1-fedramp-prod.socrata.net" ;;
+  *)
+    echo "Did not recognize stack name $STACK"
+esac
+
+if [ "$CLUSTER" == "" ] || [ "$DSID" == "" ] || [ "$COPY_NUMBER" == "" ]; then
+  echo "Usage $0 <local|staging|rc|prod|eu-prod|fedramp-prod> <dataset_id> <copy_number>"
+  exit 1
+fi
+
+
+delete_query="{\"query\":{\"bool\":{\"must\":[{\"term\":{\"dataset_id\":\"$DCU.$DSID\"}},{\"term\":{\"copy_number\":$COPY_NUMBER}}]}}}"
+echo $delete_query
+
+copy_count=$(curl -s $CLUSTER/spandex/dataset_copy/_search?size=0 -d $delete_query |jq '.hits.total')
+column_count=$(curl -s $CLUSTER/spandex/column_map/_search?size=0 -d $delete_query |jq '.hits.total')
+value_count=$(curl -s $CLUSTER/spandex/field_value/_search?size=0 -d $delete_query |jq '.hits.total')
+
+echo "found $copy_count copies, $column_count columns, $value_count values; deleting $DSID"
+curl -XDELETE $CLUSTER/spandex/_query -d $delete_query
+echo
---
+++ bin/delete_garbage_indices.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+STACK=$1
+
+case $STACK in
+  "local" )
+    DCU=primus
+    CLUSTER="http://localhost:9200" ;;
+  "staging" )
+    CLUSTER="http://spandex.elasticsearch.aws-us-west-2-staging.socrata.net" ;;
+  "rc" )
+    CLUSTER="http://spandex.elasticsearch.aws-us-west-2-rc.socrata.net" ;;
+  "prod" )
+    CLUSTER="http://spandex-5.elasticsearch.aws-us-west-2-prod.socrata.net" ;;
+  "eu-prod" )
+    CLUSTER="http://spandex.elasticsearch.aws-eu-west-1-prod.socrata.net" ;;
+  "fedramp-prod" )
+    CLUSTER="http://spandex-6.elasticsearch.aws-us-east-1-fedramp-prod.socrata.net" ;;
+  *)
+    echo "Did not recognize stack name $STACK"
+esac
+
+if [ -z $CLUSTER ]; then
+  echo "Usage $0 <local|staging|rc|prod|eu-prod|fedramp-prod>"
+  exit 1
+fi
+
+curl -s -XDELETE $CLUSTER/*.html
+curl -s -XDELETE $CLUSTER/perl
+curl -s -XDELETE $CLUSTER/spipe
