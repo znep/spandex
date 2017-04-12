6e09dd151b263ae5db5d094d7aae08eaed93c205
Megan Schneider <megan.schneider@socrata.com>
2017-04-12 14:12:07 -0700

    explicitly pass in content-type header for snapshot commands to remove warning (#105)

--- elasticsearch-snapshotter/docker/ship.d/snapshot.sh
+++ elasticsearch-snapshotter/docker/ship.d/snapshot.sh
@@ -6,11 +6,11 @@ if [ -z "$SNAPSHOT_ID" ]; then
     SNAPSHOT_ID=$(date +%Y%m%d%H%M%S)
 fi
 
-curl -v -XPUT http://$ES_HOST:$ES_PORT/_snapshot/$SNAPSHOT_NAME \
+curl -v -H 'Content-Type: application/json' -XPUT http://$ES_HOST:$ES_PORT/_snapshot/$SNAPSHOT_NAME \
      -d "{ \"type\": \"s3\", \"settings\": { \"region\": \"$AWS_REGION\", \"bucket\": \"$SNAPSHOT_NAME\", \"server_side_encryption\": \"true\" } }"
 echo
 
-curl -v -XPUT http://$ES_HOST:$ES_PORT/_snapshot/$SNAPSHOT_NAME/$SNAPSHOT_ID \
+curl -v -H 'Content-Type: application/json' -XPUT http://$ES_HOST:$ES_PORT/_snapshot/$SNAPSHOT_NAME/$SNAPSHOT_ID \
      -d "{ \"indices\": \"$INDICES\", \"ignore_unavailable\": $IGNORE_UNAVAILABLE, \"include_global_state\": $INCLUDE_GLOBAL_STATE }"
 echo
 
